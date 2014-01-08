59ca51a69c4afbf6862e3a67196d2137ce716edb
Jason Ng <ywng@ust.hk>
2014-01-09 00:41:35 +0800

    reorganize file structure

---
+++ TaxiBook/TaxiBook/ViewControllers/BookTaxi/BookTaxiMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  BookTaxiMainViewController.m
+//  TaxiBook
+//
+//  Created by Yik Wai Ng Jason on 9/1/14.
+//  Copyright (c) 2014 taxibook. All rights reserved.
+//
+
+#import "BookTaxiMainViewController.h"
+
+@interface BookTaxiMainViewController ()
+
+@end
+
+@implementation BookTaxiMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
