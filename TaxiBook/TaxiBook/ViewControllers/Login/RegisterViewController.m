b45720d8a6e75f867ee4fb46c19946a1082331c0
Jason Ng <ywng@ust.hk>
2014-04-19 18:11:03 +0800

    fix keyboards’ problem in reg form

--- TaxiBook/TaxiBook/ViewControllers/Login/RegisterViewController.m
+++ TaxiBook/TaxiBook/ViewControllers/Login/RegisterViewController.m
@@ -159,7 +159,32 @@
 //    [self.lastNameTextField setText:@"default_last"];
 //    [self.passwordTextField setText:@"passwordSecret"];
     
+    UIToolbar* numberToolbar = [[UIToolbar alloc]initWithFrame:CGRectMake(0, 0, 320, 50)];
+    numberToolbar.barStyle = UIBarStyleDefault;
+    numberToolbar.items = [NSArray arrayWithObjects:
+                           [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil],
+                           [[UIBarButtonItem alloc]initWithTitle:@"Next" style:UIBarButtonItemStyleDone target:self action:@selector(doneWithNumberPad)],
+                           nil];
+    [numberToolbar sizeToFit];
+    self.phoneTextField.inputAccessoryView = numberToolbar;
+    
 }
+
+
+-(void)doneWithNumberPad{
+    NSInteger nextTag = self.phoneTextField.tag + 1;
+    // Try to find next responder
+    UIResponder* nextResponder = [self.phoneTextField.superview viewWithTag:nextTag];
+    if (nextResponder) {
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        // Not found, so remove keyboard.
+        [self.phoneTextField resignFirstResponder];
+    }
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
